0f962a225690f92feb7b0a93877d392ea2467c63
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2018-12-08 20:00:34 +0900

    [UPDATE] upload final version

--- src/server/MainThread.java
+++ src/server/MainThread.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.sql.*;
+import java.util.*;
 
 public class MainThread implements Runnable {
 	Socket cSocket;
@@ -22,6 +23,8 @@ public class MainThread implements Runnable {
 			inFromClient = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
 			outToClient = new DataOutputStream(cSocket.getOutputStream());
 			inputData = inFromClient.readLine();
+			// debugging
+			System.out.println(inputData);
 			
 			// open database
 			try {
@@ -38,6 +41,8 @@ public class MainThread implements Runnable {
 			// main operation
 			char flag = inputData.charAt(0);
 			int dataNum = Integer.parseInt(inputData.substring(1, 2));
+			// debugging
+			System.out.println(flag + " " + dataNum);
 			int operatorIndex;
 			int[] operatorIndices;
 			String accessID;
@@ -61,9 +66,8 @@ public class MainThread implements Runnable {
 				outToClient.writeBytes(outputData);
 				break;
 			case 'b': // search operation
-				operatorIndex = inputData.indexOf('|');
-				accessID = inputData.substring(operatorIndex);
-				searchPCRoomList(accessID, address);
+				accessID = inputData.substring(2);
+				searchPCRoomList(accessID);
 				break;
 			case 'c': // exchange operation (point to time)			
 				workload = (dataNum - 2) / 2;
@@ -71,16 +75,22 @@ public class MainThread implements Runnable {
 				int[] points = new int[workload];
 				k = 1;
 				operatorIndices = new int[dataNum - 1];
+				operatorIndices[0] = inputData.indexOf("|");
 				for (int i = 1; i < dataNum - 1; i++)
-					operatorIndices[i] = inputData.substring(operatorIndices[i-1]).indexOf('|');
+					operatorIndices[i] = inputData.substring(operatorIndices[i-1] + 1).indexOf("|")
+					+ operatorIndices[i - 1] + 1;
 				accessID = inputData.substring(2, operatorIndices[0]);
 				String targetName = inputData.substring(operatorIndices[0] + 1, operatorIndices[1]);
 				for (int i = 0; i < workload; i++) {
-					pcrNames[i] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
-					k++;
-					points[i] = Integer.parseInt(inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]));
-					k++;
+					if (i != workload - 1) {
+						pcrNames[i] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
+						k++;
+						points[i] = Integer.parseInt(inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]));
+						k++;
+					}
 				}
+				pcrNames[workload - 1] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
+				points[workload - 1] = Integer.parseInt(inputData.substring(operatorIndices[k + 1] + 1));
 				pointToTime(accessID, targetName, pcrNames, points);
 				outToClient.writeBytes(outputData);
 				break;
@@ -90,15 +100,21 @@ public class MainThread implements Runnable {
 				int[] times = new int[workload];
 				k = 0;
 				operatorIndices = new int[dataNum - 1];
+				operatorIndices[0] = inputData.indexOf("|");
 				for (int i = 1; i < dataNum - 1; i++)
-					operatorIndices[i] = inputData.substring(operatorIndices[i-1]).indexOf('|');
+					operatorIndices[i] = inputData.substring(operatorIndices[i-1] + 1).indexOf("|")
+					+ operatorIndices[i - 1] + 1;
 				accessID = inputData.substring(2, operatorIndices[0]);
 				for (int i = 0; i < workload; i++) {
-					pcrNames[i] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
-					k++;
-					times[i] = Integer.parseInt(inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]));
-					k++;
+					if (i != workload - 1) {
+						pcrNames[i] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
+						k++;
+						times[i] = Integer.parseInt(inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]));
+						k++;
+					}
 				}
+				pcrNames[workload - 1] = inputData.substring(operatorIndices[k] + 1, operatorIndices[k + 1]);
+				times[workload - 1] = Integer.parseInt(inputData.substring(operatorIndices[k + 1] + 1));
 				timeToPoint(accessID, pcrNames, times);
 				outToClient.writeBytes(outputData);
 				break;
@@ -168,18 +184,20 @@ public class MainThread implements Runnable {
 				stmt = dbcon.createStatement();
 				String update = "update REGISTERED set LEFTTIME = LEFTTIME - "
 						+ times[i] + " where PCR_ID = " + pcr_id
-						+ "and USER_ID = '" + aid + "'";
+						+ " and USER_ID = '" + aid + "'";
 				stmt.executeUpdate(update);
 				
 				stmt = dbcon.createStatement();
 				update = "update REGISTERED set POINT = POINT + " + plusPoint
 						+ " where PCR_ID = " + pcr_id
-						+ "and USER_ID = '" + aid + "'";
+						+ " and USER_ID = '" + aid + "'";
 				int count = stmt.executeUpdate(update);
 				if (count == 1)
 					outputData = "success";
-				else
+				else {
 					outputData = "fail";
+					break;
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -195,7 +213,7 @@ public class MainThread implements Runnable {
 		int[] source_id = new int[points.length];
 		int plusTime = 0;
 		for (int i = 0; i < points.length; i++)
-			plusTime += points[i] - feePolicy(pcrNames[i]);
+			plusTime += points[i] - feePolicy(pcrNames[i], target);
 		
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -216,14 +234,18 @@ public class MainThread implements Runnable {
 				stmt = dbcon.createStatement();
 				String update = "update REGISTERED set POINT = POINT - "
 						+ points[i] + " where PCR_ID = " + source_id[i]
-						+ "and USER_ID = '" + aid + "'";
-				stmt.executeUpdate(update);
+						+ " and USER_ID = '" + aid + "'";
+				int counter = stmt.executeUpdate(update);
+				if (counter == 1)
+					outputData = "success";
+				else
+					outputData = "fail";
 			}
 			
 			stmt = dbcon.createStatement();
 			String update = "update REGISTERED set LEFTTIME = LEFTTIME + "
 					+ plusTime + " where PCR_ID = " + target_id
-					+ "and USER_ID = '" + aid + "'";
+					+ " and USER_ID = '" + aid + "'";
 			int count = stmt.executeUpdate(update);
 			if (count == 1)
 				outputData = "success";
@@ -236,17 +258,19 @@ public class MainThread implements Runnable {
 	}
 	
 	/* protocol code: b
-	 * parameter: user id, address info
-	 * operation: search PCRoom info for user in specific area
+	 * parameter: user id
+	 * operation: search PCRoom info for user
 	 * return: array of PCRoom name, left time, point */
 	private void searchPCRoomList(String aid) {
+		ArrayList<String> list = new ArrayList<String>();
 		
 		try {
 			Statement stmt = dbcon.createStatement();
 			ResultSet rs = null;
 			
-			String sql = "select NAME, LEFTTIME, POINT from PCROOM natural join "
-					+ "REGISTERED where USER_ID = '" + aid + "'";
+			String sql = "select NAME, LEFTTIME, POINT from PCROOM join REGISTERED "
+					+ "on PCROOM.ID = REGISTERED.PCR_ID where "+
+					"REGISTERED.USER_ID = '" + aid + "'";
 			rs = stmt.executeQuery(sql);
 			
 			while (rs.next()) {
@@ -254,10 +278,18 @@ public class MainThread implements Runnable {
 				int leftTime = rs.getInt(2);
 				int point = rs.getInt(3);
 				
-				outputData = name + "\t" + leftTime + "\t" + point +"\n";
-				outToClient.writeBytes(outputData);
+				String temp = name + "\t" + leftTime + "\t" + point;
+				list.add(temp);
 			}
 			
+			outputData = "";
+			for (int i = 0; i < list.size(); i++) {
+				outputData = outputData + list.get(i);
+				if (i != list.size() - 1)
+					outputData = outputData + ":";
+			}
+			
+			outToClient.writeBytes(outputData);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -266,14 +298,14 @@ public class MainThread implements Runnable {
 	}
 	
 	/* protocol code: none
-	 * parameter: pcroom name
+	 * parameter: pcroom name, target pcroom name
 	 * operation: calculate fee by each pcroom
 	 * return: fee */
 	private double feePolicy(String pcrName, String targetName) {
 		double fee = 0;
-		double temp;
+		double temp = 0;
 		
-		int franchise_source, franchise_destination;
+		int franchise_source = 0, franchise_destination = 0;
 		
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -282,21 +314,28 @@ public class MainThread implements Runnable {
 			String sql = "select FRANCHISE from PCROOM where NAME = '"
 					+ pcrName + "'";
 			rs = stmt.executeQuery(sql);
-			franchise_source = rs.getInt(1);
+			if (rs.next())
+				franchise_source = rs.getInt(1);
+			System.out.println("Source: " + franchise_source);
 			
 			stmt = dbcon.createStatement();
 			sql = "select FRANCHISE from PCROOM where NAME = '"
 					+ targetName + "'";
 			rs = stmt.executeQuery(sql);
-			franchise_destination = rs.getInt(1);
+			if (rs.next())
+				franchise_destination = rs.getInt(1);
+			System.out.println("Destination: " + franchise_destination);
 			
 			if (franchise_source != franchise_destination) {
 				stmt = dbcon.createStatement();
 				sql = "select FEE from PCROOM where FRANCHISE = "
 						+ franchise_destination;
 				rs = stmt.executeQuery(sql);
-				temp = rs.getDouble(1);
+				if (rs.next())
+					temp = rs.getDouble(1);
+				System.out.println("temp: " + temp);
 				fee = temp / 100.0;
+				System.out.println("fee: " + fee);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
