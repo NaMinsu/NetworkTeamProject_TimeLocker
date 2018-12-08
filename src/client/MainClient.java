bfbe257c184b1940562ebd16f159ad8ae610081f
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2018-12-08 20:03:01 +0900

    [ADD] add final version

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -8,97 +8,97 @@ public abstract class MainClient extends JFrame implements ActionListener
 {
    public static String accessID;
    public static Login loginPage;
-   public static SearchPCRoomPanel searchPage;
    public static PointPanel pointPage;
-   public static ChangePointPanel changePage;
-   public static FinalPanel finalPage;
+   public static ConfirmPanel confirmPage;
 
    public static void main(String[] args)
    {
       loginPage = new Login();
       pointPage = new PointPanel();
-      searchPage = new SearchPCRoomPanel();
-      changePage = new ChangePointPanel();
-      finalPage = new FinalPanel();
+      confirmPage = new ConfirmPanel();
 
       loginPage.setVisible(true);
-      
+
       //button actionListener
       //loginPage
-      // TODO: making login
-     loginPage.loginButton.addActionListener(new ActionListener() {
-    	 @Override
-    	 public void actionPerformed(ActionEvent e) {
-    	 accessID = loginPage.getAccessID();
-    	 loginPage.setVisible(false);
-         pointPage.setVisible(true);
-         searchPage.setVisible(false);
-         changePage.setVisible(false);
-//         finalPage.setVisible(false);
-    	 }
+      loginPage.loginButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            //PROTOCOL 합치는 과정
+            String in = (loginPage.tf.getText()).concat("|");
+            String pw = new String (loginPage.pf.getPassword());
+            String info = in.concat(pw);
+
+            loginPage.protocol = loginPage.protocol.concat(info);
+
+            loginPage.socketStarting();
+
+            if (loginPage.isLogIn()) {
+            	accessID = loginPage.getAccessID();
+            	pointPage.accessID = accessID;
+            	confirmPage.accessID = accessID;
+            	
+            	pointPage.updateList();
+            	
+            	loginPage.setVisible(false);
+                pointPage.setVisible(true);
+                confirmPage.setVisible(false);
+            }
+         }
       });
-      
+
       //pointPage
       pointPage.GoBack.addActionListener(new ActionListener() 
       {
          @Override
-    	  public void actionPerformed(ActionEvent arg0)
+         public void actionPerformed(ActionEvent arg0)
          {
             loginPage.setVisible(true);
             pointPage.setVisible(false);
-            searchPage.setVisible(false);
-            changePage.setVisible(false);
-//            finalPage.setVisible(false);
+            confirmPage.setVisible(false);
          }
       });
-      
-      pointPage.UsePoint.addActionListener(new ActionListener() 
+
+      pointPage.PointToTime.addActionListener(new ActionListener() 
       {
          public void actionPerformed(ActionEvent arg0)
          {
-            loginPage.setVisible(false);
-            pointPage.setVisible(false);
-            searchPage.setVisible(true);
-            changePage.setVisible(false);
-//            finalPage.setVisible(false);
+        	if (pointPage.setSelected()) { 
+        	    confirmPage.selected = pointPage.selected;
+        	    confirmPage.updateList();
+        		loginPage.setVisible(false);
+                pointPage.setVisible(false);
+                confirmPage.setVisible(true);
+        	}
          }
       });
-      
-      //searchPage
-      searchPage.BackToPreviousPage.addActionListener(new ActionListener() 
+
+      pointPage.TimeToPoint.addActionListener(new ActionListener() 
       {
          public void actionPerformed(ActionEvent arg0)
          {
-            loginPage.setVisible(false);
-            pointPage.setVisible(true);
-            searchPage.setVisible(false);
-            changePage.setVisible(false);
-//            finalPage.setVisible(false);
+            pointPage.pointExchanger();
+            if (pointPage.updated) {
+            	pointPage.updated = false;
+            	pointPage.setVisible(false);
+            	pointPage.updateList();
+            	pointPage.setVisible(true);
+            }
          }
       });
       
-      searchPage.GoToNextPage.addActionListener(new ActionListener() 
+      confirmPage.ConfirmButton.addActionListener(new ActionListener() 
       {
          public void actionPerformed(ActionEvent arg0)
          {
-            loginPage.setVisible(false);
-            pointPage.setVisible(false);
-            searchPage.setVisible(false);
-            changePage.setVisible(true);
-//            finalPage.setVisible(false);
-         }
-      });
-      
-      //changePage
-      changePage.ChangePoint.addActionListener(new ActionListener() 
-      {
-         public void actionPerformed(ActionEvent arg0)
-         {
-            loginPage.setVisible(false);
-            pointPage.setVisible(false);
-            searchPage.setVisible(false);
-            changePage.setVisible(false);
-//            finalPage.setVisible(true);
+        	 confirmPage.buyTime();
+        	 if (confirmPage.updated) {
+        		 confirmPage.updated = false;
+        		 pointPage.updateList();
+        		 loginPage.setVisible(false);
+        		 pointPage.setVisible(true);
+        		 confirmPage.setVisible(false);
+        	 }
          }
       });
    }
